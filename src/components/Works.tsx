0591c11046af67a08105c20ad4718051c9fef09a
Alisa03 <88279100+Alisa03@users.noreply.github.com>
2023-06-27 22:49:03 +0300

    fix

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -9,9 +9,11 @@ const Works = () => {
     return (
         <section id="works" className={`${style.section} ${style.flexCG} ${style.flexAc}`}>
             <h3 className={style.section__title}>WORKS</h3>
-            {
-                works.map((i: any) => <Project key={i.id} project={i} />)
-            }
+            <div>
+                {
+                    works.map((i: any) => <Project key={i.id} project={i} />)
+                }
+            </div>
         </section>
     )
 }
