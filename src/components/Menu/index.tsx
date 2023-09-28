3d081132f0543ec494d76174c3119ecc5e9db043
Alisa03 <88279100+Alisa03@users.noreply.github.com>
2023-09-29 00:29:22 +0300

    fix burger

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -16,9 +16,9 @@ const MobileMenu = () => {
     return (
         <>
             <MenuList styleList={menu.menu} />
-            <button onClick={Modal} className={`${style.btn} ${menu.burger} ${style.flexAc} ${open ? menu.burger__active : ''}`}>
-                <span className={menu.burger__line} />
-                <span className={menu.burger__line} />
+            <button onClick={Modal} className={`${style.btn} ${style.burger} ${style.flexAc} ${open ? style.burger__active : ''}`}>
+                <span className={style.burger__line} />
+                <span className={style.burger__line} />
             </button>
             <MenuList Click={Modal} styleList={`${menu.dropdownMenu} ${open ? menu.visible : ''}`} />
         </>
