478d45b58f4ab9f1c5d9766d0e850c10bca070a3
Alisa03 <alisa033306@gmail.com>
2024-07-27 20:31:45 +0300

    fix header mobile

--- src/screens/Header/index.tsx
+++ src/screens/Header/index.tsx
@@ -3,23 +3,21 @@ import clsx from 'clsx'
 import s from './header.module.scss'
 
 export const Header = () => {
-    return (
-        <div id='home' className={s.head}>
-            <div className={s.stars}>
-                <div className={clsx(s.star, s.star1)} />
-                <div className={clsx(s.star, s.star2)} />
-                <div className={clsx(s.star, s.star3)} />
-            </div>
-            <div className={clsx(s.content, " flex-center")}>
-                <h1 className={clsx(s.title, "color")}>
-                    Привет! Я Светлана, <br />
-                    frontend разработчик
-                </h1>
-                <img className={s.img} src='/logo.png' alt="me - Sveta" />
-            </div>
-            <div className={s.scroll_position}>
-                <a href="#about" className={s.scroll} />
-            </div>
+    return <div id='home' className={s.head}>
+        <div className={s.stars}>
+            <div className={clsx(s.star, s.star1)} />
+            <div className={clsx(s.star, s.star2)} />
+            <div className={clsx(s.star, s.star3)} />
         </div>
-    )
+        <div className={clsx(s.content, " flex-center")}>
+            <h1 className={clsx(s.title, "color")}>
+                Привет! Я Светлана, <br />
+                frontend разработчик
+            </h1>
+            <img className={s.img} src='/logo.png' alt="me - Sveta" />
+        </div>
+        <div className={s.scroll_position}>
+            <a href="#about" className={s.scroll} />
+        </div>
+    </div>
 }
